c6e19756be751fbc907668521fa90ccaec93e2d4
Paul <paulglujan@gmail.com>
2022-11-28 11:39:42 -0800

    Created cities table

---
+++ cities.sql
@@ -0,0 +1,18 @@
+CREATE TABLE
+  cities (
+    name VARCHAR(50),
+    country VARCHAR(50),
+    population INTEGER,
+    area INTEGER
+  );
+
+INSERT INTO
+  cities (name, country, population, area)
+VALUES
+  ('Tokyo', 'Japan', 37468000, 2191);
+
+INSERT INTO
+  cities (name, country, population, area)
+VALUES
+  ('Delhi', 'India', 28514000, 1484),
+  ('Shanghai', 'China', 25582000, 6341);
