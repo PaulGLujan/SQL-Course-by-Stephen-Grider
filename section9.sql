846a6d9a2e3e9a642d8197ee5e2a68f9932ff991
Paul <paulglujan@gmail.com>
2022-12-02 11:51:35 -0800

    Manual formatting

--- section9.sql
+++ section9.sql
@@ -1,31 +1,24 @@
 -- Select the highest price in the toy department.
-SELECT
-  price
-FROM
-  products
-WHERE
-  department = 'Toys'
-ORDER BY
-  price DESC
-LIMIT
-  1;
+SELECT price FROM products WHERE department = 'Toys' ORDER BY price DESC LIMIT 1;
 
 -- Select name and price of all products more expensive than most expensive toy.
-SELECT
-  name,
-  price
-FROM
-  products
-WHERE
-  price > (
-    SELECT
-      price
-    FROM
-      products
-    WHERE
-      department = 'Toys'
-    ORDER BY
-      price DESC
-    LIMIT
-      1
+SELECT name, price 
+FROM products 
+WHERE price > 
+  ( 
+    SELECT price 
+    FROM products 
+    WHERE department = 'Toys' 
+    ORDER BY price 
+    DESC LIMIT 1
   );
+
+SELECT name FROM products WHERE id = 1;
+
+SELECT name, price, 
+  ( 
+    SELECT name 
+    FROM products 
+    WHERE id = 1
+  ) 
+FROM products;
