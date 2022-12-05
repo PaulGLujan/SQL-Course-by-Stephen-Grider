34c673b4c8b4de1488e57cd66d8308cda1d768b7
Paul <paulglujan@gmail.com>
2022-12-05 13:36:16 -0800

    Finished section12 - did not add all examples

---
+++ section12-PostgreSQL-Complex-Datatypes.sql
@@ -0,0 +1,11 @@
+-- Prints 12
+SELECT (12.3242342325::INTEGER);
+
+-- Prints ads
+SELECT ('adsfdasfas'::CHAR(3));
+
+-- Prints a with whitespaces
+SELECT('a'::CHAR(3));
+
+-- Prints a without whitespaces
+SELECT('a'::VARCHAR(3));
