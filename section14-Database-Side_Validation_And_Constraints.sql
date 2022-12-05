a3451f5cf7efbc2ea7a3ec7024eb6d83494af8e5
Paul <paulglujan@gmail.com>
2022-12-05 15:58:25 -0800

    Working through section 14

--- section14-Database-Side_Validation_And_Constraints.sql
+++ section14-Database-Side_Validation_And_Constraints.sql
@@ -36,3 +36,53 @@ VALUES ('Shoes', 'Clothes', 5);
 
 -- ERROR:  null value in column "price" of relation "products" violates not-null
 -- constraint
+
+/**************************************************/
+/***           DEFAULT Column Exercise          ***/
+/**************************************************/
+
+ALTER TABLE products
+ALTER COLUMN price
+SET DEFAULT 999;
+
+-- Query returned successfully in 64 msec.
+
+INSERT INTO products (name, department, weight)
+VALUES ('Shoes', 'Clothes', 5);
+
+-- Query returned successfully in 55 msec.
+
+/**************************************************/
+/***           UNIQUE Constraint Exercise       ***/
+/**************************************************/
+
+ALTER TABLE products
+ADD UNIQUE (name);
+
+-- Query returned successfully in 273 msec.
+
+INSERT INTO products (name, department, weight)
+VALUES ('Shoes', 'Clothes', 5);
+
+-- ERROR:  duplicate key value violates unique constraint "products_name_key"
+-- DETAIL:  Key (name)=(Shoes) already exists.
+
+/**************************************************/
+/***      Multi Column Uniqueness Exercise      ***/
+/**************************************************/
+
+ALTER TABLE products
+DROP CONSTRAINT products_name_key;
+
+ALTER TABLE products
+ADD UNIQUE (name, department);
+
+INSERT INTO products (name, department, price, weight)
+VALUES ('Shirt', 'Housewares', 24, 1);
+
+-- Note a "Shirt" in the "Clothes" department already exists
+INSERT INTO products (name, department, price, weight)
+VALUES ('Shirt', 'Clothes', 24, 1);
+
+-- ERROR:  duplicate key value violates unique constraint "products_name_department_key"
+-- DETAIL:  Key (name, department)=(Shirt, Clothes) already exists.
